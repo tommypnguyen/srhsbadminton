71af03421a5864cbc8f75851b4111e6ca4dd8ec8
Tommy Nguyen <tommynguyen.164@gmail.com>
2025-03-11 23:29:31 -0700

    edit for ssqlmode

--- backend/srhsbadminton/settings.py
+++ backend/srhsbadminton/settings.py
@@ -156,8 +156,11 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
+        "OPTIONS": {
+            'sslmode': 'require',
+            }
+        },
     }
-}
 
 
 # Password validation
