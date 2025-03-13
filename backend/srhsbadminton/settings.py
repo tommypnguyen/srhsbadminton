ea57714b19104136ba90c7a66d75eff898901521
Tommy Nguyen <tommynguyen.164@gmail.com>
2025-03-13 00:06:04 -0700

    add cors credentials

--- backend/srhsbadminton/settings.py
+++ backend/srhsbadminton/settings.py
@@ -209,3 +209,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
+
+CORS_ALLOW_CREDENTIALS=True
